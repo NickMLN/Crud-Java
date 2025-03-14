94d7a550ec2b88219410c14385e857f9e62623e8
NickMLN <moreiralima.nicolas@gmail.com>
2025-03-14 09:41:07 -0300

    Add

--- src/main/java/br/senac/sp/view/ViewCliente.java
+++ src/main/java/br/senac/sp/view/ViewCliente.java
@@ -1,18 +1,44 @@
 package br.senac.sp.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ViewCliente extends JFrame {
-    public ViewCliente(){
+    JLabel lbIb;
+    JTextField tfId;
+    Font fontPadrao;
+
+    public static void main(String[] args) {
+        new ViewCliente();
+    }
+
+    public ViewCliente() {
         initComponents();
         actions();
     }
-    // Construir a Tela
-    private void initComponents(){
 
+    // Construir a Tela
+    private void initComponents() {
+        fontPadrao = new Font("Tahoma", Font.PLAIN, 12);
+        //lbId
+        lbIb = new JLabel("ID: ");
+        lbIb.setBounds(20,20,50,20);
+        lbIb.setFont(fontPadrao);
+
+        //Adiconar Componentes
+        getContentPane().add(lbIb);
+
+        //Parâmetros do frame
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setBackground(Color.BLACK);
+        setUndecorated(true);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
+
     //Definir Componentes
-    private void actions () {
+    private void actions() {
 
     }
 }
